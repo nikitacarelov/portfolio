541fb1f0997d49779ccbce27d23fbf5aa45162b6
Nikita <51008741+nikitacarelov@users.noreply.github.com>
2024-06-25 12:41:09 -0400

    Added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Nikita Carelov",
@@ -10,6 +11,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-background text-text max-w-screen-lg mx-auto font-serif">
         {children}
+        <Analytics />
+
       </body>
     </html>
   );
